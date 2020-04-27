4812364aaa83eda780063767cdd2afc4a01131ce
MarkTan <marktanjx@gmail.com>
2020-04-27 14:48:58 +0800

    Carousel Animation

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit, ViewChild } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { SessionService } from 'src/app/service/session.service';
 import { AnnouncementService } from 'src/app/service/announcement.service';
 import { Announcement } from 'src/app/classes/announcement';
+import { NgbCarousel, NgbSlideEvent, NgbSlideEventDirection } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
     selector: 'app-index',
@@ -16,15 +17,38 @@ export class IndexComponent implements OnInit {
         public sessionService: SessionService,
         public announcementService: AnnouncementService
     ) {}
+
+    @ViewChild("slider", { static: false } ) slider: ElementRef;
     announcements: Announcement[];
     staticAlertClosed = false;
+    left: boolean;
+
 
     ngOnInit() {
-        // setTimeout(() => this.staticAlertClosed = true, 20000);
+        setTimeout(() => this.staticAlertClosed = true, 20000);
         this.announcementService
             .retrieveAllAnnouncements()
             .subscribe((response) => {
                 this.announcements = response.announcements;
-            });
+
+            }),
+            
+            (error) => {
+                console.log(error);
+            };
+    }
+    changeSlide(event: NgbSlideEvent){
+        if(event.direction == NgbSlideEventDirection.LEFT){
+            this.left = null     
+            this.left = true;
+            console.log('left')
+            // @HostBinding 
+            // dom.queryselector
+            
+        } else{
+            this.left = null
+            this.left = false      
+            console.log('right')
+        }
     }
 }
